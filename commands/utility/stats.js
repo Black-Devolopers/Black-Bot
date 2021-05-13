3ca75e2f6c69412991dd70636512d1338d85784e
Msv <78690237+MSVFORYOU@users.noreply.github.com>
2021-05-13 14:47:44 +0530

    Create stats.js

---
+++ commands/utility/stats.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const { stripIndent } = require('common-tags');
+
+module.exports = {
+    config: {
+        name: "stats",
+        aliases: ['bot-info'],
+        category: "info",
+        description: "Shows Bot Statistics",
+        usage: "",
+   },
+    run: async (client, message, args) => {
+        
+    const d = moment.duration(client.uptime);
+    const days = (d.days() == 1) ? `${d.days()} day` : `${d.days()} days`;
+    const hours = (d.hours() == 1) ? `${d.hours()} hour` : `${d.hours()} hours`;
+    const seconds = (d.seconds() == 1) ? `${d.seconds()} seconds` : `${d.seconds()} seconds`;
+    const minutes = (d.minutes() == 1) ? `${d.minutes()} minutes` : `${d.minutes()} minutes`;
+    const clientStats = stripIndent`
+      Servers   :: ${client.guilds.cache.size}
+      Users     :: ${client.guilds.cache.reduce(
+    (prev, guild) => prev + guild.memberCount, 0)}
+      Channels  :: ${client.channels.cache.size}
+      WS Ping   :: ${Math.round(client.ws.ping)}ms
+      Uptime    :: ${days} and ${hours} and ${minutes} and ${seconds}
+    `;
+    
+    const embed = new MessageEmbed()
+      .setTitle('Bot\'s Statistics')
+      .setDescription(`\`\`\`asciidoc\n${clientStats}\`\`\``)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+    }
+}
