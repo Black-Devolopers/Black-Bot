3a03b2884740fa4a1a692b9c9b0e4137698e927d
Msv <78690237+MSVFORYOU@users.noreply.github.com>
2021-05-13 14:39:38 +0530

    Create invite.js

---
+++ commands/utility/invite.js
@@ -0,0 +1,37 @@
+var Discord = require('discord.js')
+const fs = require("fs")
+const { PREFIX } = require("../../config")
+const db = require('quick.db')
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+config: {
+    name: "invite",
+    description: "invite link of bot",
+    usage: "1) m/invite \n2) m/inv",
+    aliases: ['i']
+},
+run: async (bot, message, args) => {
+    let prefix;
+    if (message.author.bot || message.channel.type === "dm") return;
+        try {
+            let fetched = await db.fetch(`prefix_${message.guild.id}`);
+            if (fetched == null) {
+                prefix = PREFIX
+            } else {
+                prefix = fetched
+            }
+        } catch (e) {
+            console.log(e)
+    };
+
+if(message.content.toLowerCase() === `${prefix}invite`){
+    var log = new Discord.MessageEmbed()
+    .setColor(`#dcf104`)
+    .setAuthor("BOT SUPPORT")
+    .setDescription("[**CLICK HERE TO INVITE ME**](https://discord.gg/7pgJQqMjDQ)",true)
+
+message.channel.send(log);
+}
+}
+}
